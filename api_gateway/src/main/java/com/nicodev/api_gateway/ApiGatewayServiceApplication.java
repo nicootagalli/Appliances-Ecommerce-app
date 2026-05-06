80cc321922ed0e01106aae1feb7453a0f25e2a94
nicootagalli <nicootagallik1@gmail.com>
2026-05-06 16:35:56 -0300

    update

--- api_gateway/src/main/java/com/nicodev/api_gateway/ApiGatewayServiceApplication.java
+++ api_gateway/src/main/java/com/nicodev/api_gateway/ApiGatewayServiceApplication.java
@@ -1,7 +1,8 @@
-package com.nicodev.api_gateway_service;
+package com.nicodev.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
